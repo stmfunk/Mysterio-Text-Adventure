ed9d7e2805950c27245dc219a90e2eca11926921
Donal O'Shea <donaloshea@outlook.com>
2020-07-12 10:19:37 +0100

    Initial Commit. Game complete (lol)

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // SerializeField allows variable to accessed in Unity Inspector
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStoryText();
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        ManageState();
+        
+    }
+
+    private void ManageState()
+    {
+        if (Input.anyKeyDown) { 
+			int index;
+			if (Input.GetKeyDown(KeyCode.Alpha1)) index = 0;
+			else if (Input.GetKeyDown(KeyCode.Alpha2)) index = 1;
+			else index = 2;
+
+			try
+			{
+			    state = state.NextState(index);
+
+			    LoadNewState(state);
+
+			} catch (IndexOutOfRangeException e) { 
+			    Debug.Log(e);
+			}
+
+        }
+    }
+
+    private void LoadNewState(State state) {
+        textComponent.text = state.GetStoryText();
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)][SerializeField]  string storyText;
+    [SerializeField]  State[] nextStates;
+ 
+    public string GetStoryText()
+    {
+        return storyText;
+    }
+
+    public virtual State NextState(int stateNum)
+    {
+        if (nextStates.Length > stateNum)
+            return nextStates[stateNum];
+        else
+        {
+            throw new IndexOutOfRangeException("Value too low");
+        }
+    }
+  }
